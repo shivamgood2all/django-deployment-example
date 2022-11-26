33092760ef0ab1b701dd526221282ff2c173ef8d
Shivam Kumar <shivam.goodtoall@gmail.com>
2022-11-26 15:14:44 +0530

    'first'

--- learning_templates/learning_templates/settings.py
+++ learning_templates/learning_templates/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-hg68f$zj()=g*cqkm8at#d1#sray^67bcnu#^xsk@=lry&i3on
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['shivamgood2all.pythonanywhere.com',]
 
 
 # Application definition
